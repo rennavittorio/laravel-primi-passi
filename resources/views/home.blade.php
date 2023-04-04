4d07485fd43fd5508b10b7bec15014c4f8dff541
rennavittorio <renna.vittorio@gmail.com>
2023-04-04 16:08:17 +0200

    add: add homepage view and redirect initial route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- lang="{{ str_replace('_', '-', app()->getLocale()) }}" -->
+<!-- to add on lang -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container text-align-center">
+
+        hello world!
+
+    </div>
+
+</body>
+
+</html>
